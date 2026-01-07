9613a2a1babd10a2826ef705c099c8072142d463
d41dev <d41dev@proton.me>
2026-01-07 09:23:24 +0000

    update: add functions to calculate embeddin cost

---
+++ revelium/tokens.py
@@ -0,0 +1,22 @@
+import tiktoken
+from smartscan import ModelName
+from smartscan.providers import MiniLmTextEmbedder
+from server.constants import MINILM_MODEL_PATH
+from revelium.prompts.types import Prompt
+
+
+def count_tokens_embedding(text: str, model: str | ModelName = "text-embedding-3-large") -> int:
+    if model == "all-minilm-l6-v2":
+        text_embedder = MiniLmTextEmbedder(MINILM_MODEL_PATH)
+        padded_tkns = text_embedder._tokenize(text)
+        return len([token for token in padded_tkns if token != 0])
+    else:
+        enc = tiktoken.encoding_for_model(model)
+        return len(enc.encode(text))
+
+
+def embedding_token_cost(text: str, price_per_1k_tokens: float,model: str = "text-embedding-3-large") -> float:
+    tokens = count_tokens_embedding(text, model)
+    return tokens * (price_per_1k_tokens / 1000)
+    
+
