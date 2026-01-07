51c2fddbc7811ab090bdb327d65cc9aad0e27ab3
d41dev <d41dev@proton.me>
2026-01-07 08:57:21 +0000

    update: import asyncsqlitestore

--- revelium/prompts/store/__init__.py
+++ revelium/prompts/store/__init__.py
@@ -1,5 +1,5 @@
 from revelium.prompts.types import Prompt 
 from revelium.prompts.store.prompt_store import PromptStore
 from revelium.prompts.store.in_memory_prompt_store import InMemoryPromptStore
-from revelium.prompts.store.sqlite_prompt_store import SQLitePromptStore
+from revelium.prompts.store.sqlite_prompt_store import AsyncSQLitePromptStore
 
