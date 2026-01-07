4c9fa24e9c865682814268e27efb04fb1d3f25e3
d41dev <d41dev@proton.me>
2026-01-07 01:33:30 +0000

    update: get prompt from prompt store

--- revelium/indexer.py
+++ revelium/indexer.py
@@ -3,11 +3,13 @@ from smartscan.utils import chunk_text
 from smartscan.embeddings import generate_prototype_embedding
 from smartscan.processor import BatchProcessor, ProcessorListener
 from smartscan.providers import TextEmbeddingProvider
+from revelium.prompts.store import PromptStore
 
-class PromptIndexer(BatchProcessor[tuple[str, str], ItemEmbedding]):
+class PromptIndexer(BatchProcessor[str, ItemEmbedding]):
     def __init__(self, 
                 text_encoder: TextEmbeddingProvider,
                 max_tokenizer_length: int,
+                prompt_store: PromptStore,
                 max_chunks: int | None = None,
                 **kwargs
                 ):
@@ -16,20 +18,26 @@ class PromptIndexer(BatchProcessor[tuple[str, str], ItemEmbedding]):
         self.max_chunks = max_chunks
         self.max_tokenizer_length = max_tokenizer_length
         self.item_embeddings = []
+        self.prompt_store = prompt_store
 
     # All chunks share the same item_id (url or file) so that chunks are group
     # In the on_batch_complete method, the listener can handle use it as metaddata and assign unique ids to each chunk if required
     def on_process(self, item):
-        chunks = chunk_text(item[1], self.max_tokenizer_length)
+        prompts = self.prompt_store.get_by_ids(ids=[item])
+        if len(prompts) == 0:
+            raise ValueError("Prompt not found")
+        
+        prompt = prompts[0]
+        chunks = chunk_text(prompt.content, self.max_tokenizer_length)
         embeddings = self.text_encoder.embed_batch(chunks)
         text_prototype = generate_prototype_embedding(embeddings)
-        return ItemEmbedding(item[0], text_prototype)
+        return ItemEmbedding(prompt.prompt_id, text_prototype)
              
-    # delegate to lister e.g to handle storage
+    # TODO: add to EmbeddingStore
     async def on_batch_complete(self, batch):
         self.item_embeddings.extend( batch)
         
 
-class DefaultPromptIndexerListener(ProcessorListener[tuple[str, str], ItemEmbedding]):
+class DefaultPromptIndexerListener(ProcessorListener[str, ItemEmbedding]):
     def on_error(self, e, item):
         print(e)
