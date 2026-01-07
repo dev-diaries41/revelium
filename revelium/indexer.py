775dec83315dde19d8ad9e75c0739606084fe93f
d41dev <d41dev@proton.me>
2026-01-07 09:18:58 +0000

    update: accept Prompt instead of prompt_id

--- revelium/indexer.py
+++ revelium/indexer.py
@@ -4,8 +4,9 @@ from smartscan.embeddings import generate_prototype_embedding
 from smartscan.processor import BatchProcessor, ProcessorListener
 from smartscan.providers import TextEmbeddingProvider
 from revelium.prompts.store import PromptStore
+from revelium.prompts.types import Prompt
 
-class PromptIndexer(BatchProcessor[str, ItemEmbedding]):
+class PromptIndexer(BatchProcessor[Prompt, ItemEmbedding]):
     def __init__(self, 
                 text_encoder: TextEmbeddingProvider,
                 max_tokenizer_length: int,
@@ -23,21 +24,16 @@ class PromptIndexer(BatchProcessor[str, ItemEmbedding]):
     # All chunks share the same item_id (url or file) so that chunks are group
     # In the on_batch_complete method, the listener can handle use it as metaddata and assign unique ids to each chunk if required
     def on_process(self, item):
-        prompts = self.prompt_store.get_by_ids(ids=[item])
-        if len(prompts) == 0:
-            raise ValueError("Prompt not found")
-        
-        prompt = prompts[0]
-        chunks = chunk_text(prompt.content, self.max_tokenizer_length)
+        chunks = chunk_text(item.content, self.max_tokenizer_length)
         embeddings = self.text_encoder.embed_batch(chunks)
         text_prototype = generate_prototype_embedding(embeddings)
-        return ItemEmbedding(prompt.prompt_id, text_prototype)
+        return ItemEmbedding(item.prompt_id, text_prototype)
              
     # TODO: add to EmbeddingStore
     async def on_batch_complete(self, batch):
         self.item_embeddings.extend( batch)
         
 
-class DefaultPromptIndexerListener(ProcessorListener[str, ItemEmbedding]):
+class DefaultPromptIndexerListener(ProcessorListener[Prompt, ItemEmbedding]):
     def on_error(self, e, item):
         print(e)
