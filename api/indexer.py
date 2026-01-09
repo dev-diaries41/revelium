8f714fbe1705249d2b71a4a1e4eeac8d5aac4598
d41dev <d41dev@proton.me>
2026-01-09 21:39:03 +0000

    uodate: extract base ws listener

--- api/indexer.py
+++ api/indexer.py
@@ -1,81 +1,45 @@
 from fastapi import WebSocket
-from pydantic import BaseModel
-from typing import Literal
-from starlette.websockets import WebSocketState
-
-from chromadb import Collection
 
 from smartscan.processor import ProcessorListener
+from smartscan.processor.types import Input, Output
 from smartscan.types import ItemEmbedding
+from smartscan.embeds import EmbeddingStore
 
-class ActiveMessage(BaseModel):
-    type: Literal["active"] = "active"
-    
-class ProgressMessage(BaseModel):
-    type: Literal["progress"] = "progress"
-    progress: float
+from revelium.schemas.api import FailMessage, ErrorMessage, ProgressMessage, CompleteMessage, ActiveMessage
 
-class ErrorMessage(BaseModel):
-    type: Literal["error"] = "error"
-    error: str
-    item: str
 
-class FailMessage(BaseModel):
-    type: Literal["fail"] = "fail"
-    error: str
 
-class CompleteMessage(BaseModel):
-    type: Literal["complete"] = "complete"
-    total_processed: int
-    time_elapsed: float
-
-class FileIndexerWebSocketListener(ProcessorListener[str, ItemEmbedding]):
-    def __init__(
-            self, 
-            ws: WebSocket,
-            store: Collection,
-            ):
+class BaseWebSocketListener(ProcessorListener[Input, Output]):
+    def __init__(self, ws: WebSocket):
         self.ws = ws
-        self.store = store
 
     async def on_active(self):
-        if self.ws.client_state == WebSocketState.CONNECTED:
-            try:
-                await self.ws.send_json(ActiveMessage().model_dump())  
-            except RuntimeError:
-                pass 
+        await self.ws.send_json(ActiveMessage().model_dump())  
+
 
     async def on_progress(self, progress):
-        if self.ws.client_state == WebSocketState.CONNECTED:
-            try:
-                await self.ws.send_json(ProgressMessage(progress=progress).model_dump())  
-            except RuntimeError:
-                pass      
+        await self.ws.send_json(ProgressMessage(progress=progress).model_dump())  
+    
     
     async def on_fail(self, result):
-        if self.ws.client_state == WebSocketState.CONNECTED:
-            try:
-                await self.ws.send_json(FailMessage(error=str(result.error)).model_dump())
-            except RuntimeError:
-                pass
+        await self.ws.send_json(FailMessage(error=str(result.error)).model_dump())
+
 
     async def on_error(self, e, item):
-        if self.ws.client_state == WebSocketState.CONNECTED:
-            try:
-                await self.ws.send_json(ErrorMessage(error=str(e), item=item).model_dump())
-            except RuntimeError:
-                pass
+        await self.ws.send_json(ErrorMessage(error=str(e), item=item).model_dump())
+
 
     async def on_complete(self, result):
-        if self.ws.client_state == WebSocketState.CONNECTED:
-            try:
-                await self.ws.send_json(CompleteMessage(total_processed=result.total_processed, time_elapsed=result.time_elapsed).model_dump())
-            except RuntimeError:
-                pass
+        await self.ws.send_json(CompleteMessage(total_processed=result.total_processed, time_elapsed=result.time_elapsed).model_dump())
+
+
+
+class PromptIndexerWebSocketListener(ProcessorListener[str, ItemEmbedding]):
+    def __init__(self, cluster_embedding_store: EmbeddingStore, kwargs):
+        super().__init__(**kwargs)
+        self.cluster_embedding_store = cluster_embedding_store
 
     async def on_batch_complete(self, batch):
         if len(batch) <= 0:
             return
-        ids, embeddings = zip(*((item.item_id, item.embedding) for item in batch))
-        self.store.add(ids = ids, embeddings=embeddings)
-
+        self.cluster_embedding_store.add(batch)
