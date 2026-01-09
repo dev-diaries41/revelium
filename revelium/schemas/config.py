f4f516c511f6a3f44311bf9d1b8f23fce4d49102
d41dev <d41dev@proton.me>
2026-01-09 18:27:04 +0000

    update: add revelium config

---
+++ revelium/schemas/config.py
@@ -0,0 +1,15 @@
+from typing import Optional
+from pydantic import  BaseModel, Field
+from revelium.providers.types import TextEmbeddingModel
+from openai.types import ResponsesModel
+from revelium.constants import DEFAULT_CHROMADB_PATH, DEFAULT_OPENAI_MODEL, DEFAULT_PROMPTS_PATH, DEFAULT_SYSTEM_PROMPT
+
+class ReveliumConfig(BaseModel):
+    api_key: Optional[str] = Field(default=None)
+    text_embedder: TextEmbeddingModel = Field(default="all-minilm-l6-v2")
+    provider_model: ResponsesModel = Field(default=DEFAULT_OPENAI_MODEL)
+    provider_api_key: Optional[str] = Field(default=None)
+    chromadb_path: str = Field(default=DEFAULT_CHROMADB_PATH)
+    prompt_store_path: str = Field(default=DEFAULT_PROMPTS_PATH)
+    system_prompt: str = Field(default=DEFAULT_SYSTEM_PROMPT)
+    benchmarking: bool = Field(default=False)
