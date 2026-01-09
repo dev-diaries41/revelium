f2267225ed8a3df02cfc8237dabc6580c2746693
d41dev <d41dev@proton.me>
2026-01-09 10:02:32 +0000

    feat: add initial llm integration

---
+++ revelium/providers/llm/llm_client.py
@@ -0,0 +1,20 @@
+from abc import abstractmethod, ABC
+from typing import List, Optional, Dict, TypeVar
+from pydantic import BaseModel
+from revelium.schemas.model import Message
+import numpy as np
+
+JsonOutput = TypeVar("JsonOutput", bound=BaseModel)
+
+class LLMClient(ABC):
+    @abstractmethod
+    def generate_text(self, prompt: str, history: Optional[List[Message]] = None) -> str:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def generate_json(self, prompt: str, format: JsonOutput, history: Optional[List[Message]] = None) -> JsonOutput: 
+        raise NotImplementedError
+    
+    @abstractmethod
+    def generate_text_from_image(self, prompt: str, images: List[Dict[str, str]], history: Optional[List[Message]] = None) -> np.ndarray:
+        raise NotImplementedError
