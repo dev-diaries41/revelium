38e7ecd81127d2c2e31bf0b1736cb022329056e5
d41dev <d41dev@proton.me>
2026-01-09 18:27:30 +0000

    update: constants

--- revelium/constants.py
+++ revelium/constants.py
@@ -1,13 +1,11 @@
 import os 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Models
 MINILM_MODEL_PATH = os.path.join(BASE_DIR, 'models/minilm_sentence_transformer_quant.onnx')
-
-# DB
 DB_DIR = os.path.join(BASE_DIR, "db")
-# Config
-SMARTSCAN_CONFIG_PATH = os.path.join(BASE_DIR, "smartscan.json")
-
-
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+DEFAULT_SYSTEM_PROMPT = "Your objective is to label prompt messages from clusters and label them, returning ClassificationResult. Labels should be one word max 3 words."
+DEFAULT_CHROMADB_PATH = os.path.join(DB_DIR, "revelium_chromadb")
+DEFAULT_PROMPTS_PATH = os.path.join(DB_DIR, "prompts.db")
+DEFAULT_OPENAI_MODEL = "gpt-5-mini"
+MINILM_MAX_TOKENS = 512
