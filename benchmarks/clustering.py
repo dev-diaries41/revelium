9c0dfc9faf3b74345ecdd1b4183d391f396d7904
d41dev <d41dev@proton.me>
2026-01-09 12:12:53 +0000

    update: update prompts in test and use revelium client

--- benchmarks/clustering.py
+++ benchmarks/clustering.py
@@ -2,6 +2,7 @@ import random
 import json
 import chromadb
 import os
+import asyncio
 
 from dotenv import load_dotenv
 
@@ -9,10 +10,10 @@ load_dotenv()
 
 from dataclasses import asdict
 from smartscan import  ItemId, ClusterResult
-from smartscan.classify import IncrementalClusterer, calculate_cluster_accuracy
-from revelium.embeddings.chroma_store import ChromaDBEmbeddingStore
+from smartscan.classify import IncrementalClusterer
 from revelium.utils.decorators import with_time
-from benchmarks.helpers import get_collection_name
+from benchmarks.helpers import get_collection_name, get_revelium_client
+from revelium.api.local import Revelium
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 BENCHMARK_DIR = "output/benchmarks"
@@ -31,62 +32,59 @@ def cluster(clusterer: IncrementalClusterer, ids, embeddings) -> tuple[ClusterRe
 
 # `prompt_id` must be prefixed with label e.g promptlabel_123
 # this is only for benchmarking
-def run(clusterer: IncrementalClusterer, clusters_info: dict[str, int]):
+async def run(revelium: Revelium, model: str):
     results = {}
+    revelium.clusterer.clear()
+    ## NOTE: IncrementalClusterer uses random numbers internally. Running multiple models sequentially 
+    # without reseeding causes non-deterministic clustering and lower accuracy. Reseed Python and 
+    # before each clustering run to ensure reproducible results.
 
-    for model, dim in clusters_info.items():
-        clusterer.clear()
-        ## NOTE: IncrementalClusterer uses random numbers internally. Running multiple models sequentially 
-        # without reseeding causes non-deterministic clustering and lower accuracy. Reseed Python and 
-        # before each clustering run to ensure reproducible results.
+    random.seed(32)
 
-        random.seed(32)
+    # Ensure collection name is unique per model/dim
+    count = revelium.embedding_store.count()
+    ids, embeddings = [], []
 
-        client = chromadb.PersistentClient(path=BENCHMARK_CHROMADB_PATH, settings=chromadb.Settings(anonymized_telemetry=False))
-        # Ensure collection name is unique per model/dim
-        collection_name = get_collection_name(model, dim)
-        collection = client.get_or_create_collection(name=collection_name)
-        embedding_store = ChromaDBEmbeddingStore(collection)
-        count = embedding_store.count()
-        ids, embeddings = [], []
+    limit = 500
+    offset = 0
 
-        limit = 500
-        offset = 0
+    while len(ids) < count:
+        query_result = revelium.embedding_store.get(include=['embeddings'], offset=offset, limit=limit)
+        ids.extend(query_result.ids)
+        embeddings.extend(query_result.embeddings)
+        offset += limit
 
-        while len(ids) < count:
-            query_result = embedding_store.get(include=['embeddings'], offset=offset, limit=limit)
-            ids.extend(query_result.ids)
-            embeddings.extend(query_result.embeddings)
-            offset += limit
+    result, time = cluster(revelium.clusterer, ids, embeddings)
+    # for c in result.clusters.values():
+    #     print(c.metadata)
+    
+    await revelium.update_prompts(result.assignments, result.merges)
 
-        result, time = cluster(clusterer, ids, embeddings)
-        # for c in result.clusters.values():
-        #     print(c.metadata)
+    true_labels: dict[ItemId, str] = {}
+    for prompt_id in ids:
+        label = prompt_id.split("_")[0]
+        if not label: 
+            print(f"[WARNING] {prompt_id} is not a valid labelled item.")
+            continue
+        true_labels[prompt_id] = label
 
-        true_labels: dict[ItemId, str] = {}
-        for prompt_id in ids:
-            label = prompt_id.split("_")[0]
-            if not label: 
-                print(f"[WARNING] {prompt_id} is not a valid labelled item.")
-                continue
-            true_labels[prompt_id] = label
+    acc_info = revelium.calculate_cluster_accuracy(true_labels, result.assignments)
+    bench = {"accuracy": asdict(acc_info), "clustering_speed": time}
+    results[model] = bench
+    print(f"{model}: {bench}")
 
-        acc_info = calculate_cluster_accuracy(true_labels, result.assignments)
-        bench = {"accuracy": asdict(acc_info), "clustering_speed": time}
-        results[collection_name] = bench
-        print(f"{collection_name}: {bench}")
+    with open(BENCHMARK_OUTPUT_PATH, "a") as f:
+        f.write(json.dumps(results, indent=None) + "\n")
 
-        with open(BENCHMARK_OUTPUT_PATH, "a") as f:
-            f.write(json.dumps(results, indent=None) + "\n")
-
-        # Save last result assignments
-        with open(BENCHMARK_ASSIGNMENTS_PATH, "w") as f:
-            json.dump(result.assignments, f, indent=1, sort_keys=True)
+    # Save last result assignments
+    with open(BENCHMARK_ASSIGNMENTS_PATH, "w") as f:
+        json.dump(result.assignments, f, indent=1, sort_keys=True)
 
 
-def main():
-    clusterer = IncrementalClusterer(default_threshold=0.55, sim_factor=0.9, benchmarking=True)
-    run(clusterer, {"minilm": 384})
+async def main():
+    client = chromadb.PersistentClient(path=BENCHMARK_CHROMADB_PATH, settings=chromadb.Settings(anonymized_telemetry=False))
+    collection_name = get_collection_name("minilm", 384)
+    revelium_client = get_revelium_client(client, BENCHMARK_PROMPT_STORE_PATH, collection_name)
+    await run(revelium_client, "minilm")
 
-
-main()
+asyncio.run(main())
