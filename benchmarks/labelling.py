0bd36ced36a7dfc3b6bc3256956e3a5d647b63aa
d41dev <d41dev@proton.me>
2026-01-14 19:57:47 +0000

    update: refacor labelling bench

--- benchmarks/labelling.py
+++ benchmarks/labelling.py
@@ -6,12 +6,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from benchmarks.constants import BENCHMARK_CHROMADB_PATH, BENCHMARK_PROMPT_STORE_PATH, BENCHMARK_DIR
-from revelium.constants import DEFAULT_SYSTEM_PROMPT
+from benchmarks.constants import BENCHMARK_CHROMADB_PATH, BENCHMARK_DIR
+from revelium.constants.llms import DEFAULT_SYSTEM_PROMPT
 from revelium.core.engine import Revelium, ReveliumConfig
 from revelium.providers.llm.openai import OpenAIClient
 from revelium.schemas.llm import LLMClientConfig
 from revelium.constants.llms import OPENAI_API_KEY
+from revelium.core.engine import Revelium
+from revelium.embeddings.helpers import get_embedding_store
+
 
 BENCHMARK_OUTPUT_PATH = os.path.join(BENCHMARK_DIR, "labelling_benchmarks.jsonl")
 
@@ -27,8 +30,10 @@ async def run(revelium: Revelium, cluster_id: str, sample_size: int):
 
 
 async def main():
+    prompt_embedding_store =  get_embedding_store(BENCHMARK_CHROMADB_PATH, Revelium.PROMPT_TYPE, 'all-minilm-l6-v2', 384) 
+    cluster_embedding_store =  get_embedding_store(BENCHMARK_CHROMADB_PATH, Revelium.CLUSTER_TYPE, 'all-minilm-l6-v2', 384) 
     llm = OpenAIClient(OPENAI_API_KEY, LLMClientConfig(model_name=ReveliumConfig.DEFAULT_OPENAI_MODEL, system_prompt=DEFAULT_SYSTEM_PROMPT))
-    revelium = Revelium(config=ReveliumConfig(benchmarking=True, chromadb_path=BENCHMARK_CHROMADB_PATH, prompt_store_path=BENCHMARK_PROMPT_STORE_PATH), llm_client=llm)
+    revelium = Revelium(llm_client=llm, prompt_embedding_store=prompt_embedding_store, cluster_embedding_store=cluster_embedding_store)
     await run(revelium, "fce4cfdc44b3ea3f", 10)
 
 if __name__ == "__main__":
